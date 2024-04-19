6d4e744035ea31595b1ff1e54af022c7d725d314
Manuel Sousa <mlrcbsousa@gmail.com>
2024-04-19 13:00:10 +0200

    Add ex03 projection_life.py

--- ex03/projection_life.py
+++ ex03/projection_life.py
@@ -1,30 +1,28 @@
 
 import matplotlib.pyplot as plt
+import pandas as pd
 from load_csv import load
 
 
-def main():
-    '''displays a scatter plot of with GDP on the x-axis and life expectancy
-    on the y-axis
-    '''
+def plot_gdp_life_expectancy(
+        gdp: pd.DataFrame,
+        life_expectancy: pd.DataFrame,
+        year: str
+        ):
+    """Plot the GDP and life expectancy trends of all countries."""
 
     try:
         # Data
-        df_life_expectancy = load("life_expectancy_years.csv")
-        df_gdp = load(
-            "income_per_person_gdppercapita_ppp_inflation_adjusted.csv"
-            )
-
-        series_life_expectancy_1900 = df_life_expectancy['1900']
-        series_gdp_1900 = df_gdp['1900']
+        series_gdp = gdp[year]
+        series_life_expectancy = life_expectancy[year]
 
         # Plot
-        plt.scatter(series_gdp_1900, series_life_expectancy_1900)
+        plt.scatter(series_gdp, series_life_expectancy)
         plt.xlabel('Gross Domestic Product')
         plt.ylabel('Life Expectancy')
         plt.xscale('log')
         plt.xticks([300, 1000, 10000], ['300', '1k', '10k'])
-        plt.title('1900')
+        plt.title(year)
         plt.show()
 
     # occurs if terminate the script prematurely, by pressing Ctrl+C
@@ -33,6 +31,22 @@ def main():
 
     except Exception as e:
         print(f"Error processing data: {e}")
+
+
+def main():
+    """Load GDP and life expectancy data and display for year 1900."""
+
+    df_life_expectancy = load("life_expectancy_years.csv")
+    df_gdp = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+
+    try:
+        assert df_life_expectancy is not None and df_gdp is not None
+
+    except AssertionError:
+        print("Failure to load data.")
+        return
+
+    plot_gdp_life_expectancy(df_gdp, df_life_expectancy, '1900')
 
 
 if __name__ == "__main__":
