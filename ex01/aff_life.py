1e7b2f93d52a9bd9eefa05b03871979d3b9ead70
Manuel Sousa <mlrcbsousa@gmail.com>
2024-04-19 12:42:27 +0200

    Add ex02 aff_pop.py

--- ex01/aff_life.py
+++ ex01/aff_life.py
@@ -1,6 +1,33 @@
 
 from matplotlib import pyplot as plt
 from load_csv import load
+import pandas as pd
+
+
+def plot_life_expectancy(df: pd.DataFrame, country: str):
+    """Plot the life expectancy trends of a country."""
+
+    try:
+        # Data
+        country_data = df[df['country'] == country]
+        years = df.columns[1:]
+        life_expectancy = country_data.iloc[0, 1:]
+
+        # Plot
+        plt.plot(years, life_expectancy)
+        plt.xticks(['1800', '1840', '1880', '1920',
+                    '1960', '2000', '2040', '2080'])
+        plt.xlabel('Year')
+        plt.ylabel('Life Expectancy')
+        plt.title(f"{country} Life Expectancy Projections")
+        plt.show()
+
+    # occurs if terminate the script prematurely, by pressing Ctrl+C
+    except KeyboardInterrupt:
+        pass
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def main():
@@ -9,31 +36,13 @@ def main():
     df = load("life_expectancy_years.csv")
 
     try:
-        assert df is not None, "Failure to load data."
-    except AssertionError as e:
-        print(f"AssertionError: {e}")
+        assert df is not None
+
+    except AssertionError:
+        print("Failure to load data.")
         return
 
-    try:
-        # Get the data for Belgium
-        country_data = df[df['country'] == 'Belgium']
-        years = df.columns[1:]
-        life_expectancy = country_data.iloc[0, 1:]
-
-        # Plot the data
-        plt.plot(years, life_expectancy)
-        plt.xticks(['1800', '1840', '1880', '1920',
-                    '1960', '2000', '2040', '2080'])
-        plt.xlabel('Year')
-        plt.ylabel('Life Expectancy')
-        plt.title('Belgium Life Expectancy Projections')
-        plt.show()
-
-    except KeyboardInterrupt:
-        pass
-
-    except Exception as e:
-        print(f"Error: {e}")
+    plot_life_expectancy(df, 'Belgium')
 
 
 if __name__ == "__main__":
